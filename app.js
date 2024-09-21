06ae74d312f17609d794721dad76055707f713bb
mithunshivuwork <mithunshivu.work@gmail.com>
2024-09-21 10:59:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "h1 tag"),
+    React.createElement("h2", {}, "h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "h1 tag"),
+    React.createElement("h2", {}, "h2 tag"),
+  ]),
+]);
+
+const heading = React.createElement(
+  "h1",
+  {
+    id: "heading",
+  },
+  "hello world from mithun"
+);
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent, heading);
